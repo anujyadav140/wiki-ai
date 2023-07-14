a41acfc0bc580b25e82d4ca880645ae1169dd398
Anuj <anujyadav140@gmail.com>
2023-07-14 20:17:05 +0530

    vercel deploy

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Login from "./components/authentication/login";
 import MainContent from "./components/content";
 import Tab from "./components/tabs/tab";
 import HomePage from "./pages/home";
-import ScrapeData from "./test";
 // import WikiAiPage from "./pages/wikipedia";
 
 export default function Home() {
